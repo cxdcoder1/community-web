6a992ed3b8d15a25d759427b5b5a6d21a5270827
cxd <2787450838@qq.com>
2023-09-14 13:09:57 +0800

    web初始化

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import UserLogin from "@/views/UserLogin";
+import SysWelcome from "@/views/SysWelcome";
+
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/', redirect: '/login'},
+  {path: '/login', component: UserLogin},
+  {
+    path: '/home', component: SysWelcome,
+    // children: [
+    // ]
+  },
+]
+
+
+const router = new VueRouter({
+  routes
+})
+
+// //挂载路由导航守卫,to表示将要访问的路径，from表示从哪里来，next是下一个要做的操作
+// router.beforeEach((to, from, next) => {
+//   if (to.path === '/login')
+//     return next();
+//
+//   //获取token
+//   const tokenStr = window.sessionStorage.getItem('token');
+//
+//   if (!tokenStr)
+//     return next('/login');
+//
+//   next();
+//
+// })
+
+export default router
---
+++ src/views/SysWelcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    home页
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SysWelcome"
+}
+</script>
+
+<style scoped>
+
+</style>
