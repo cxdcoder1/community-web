5d1df0a3b28d4d96fa512296b29a680ee78de3ef
whx <1060453316@qq.com>
2023-10-06 17:55:24 +0800

    接口

--- src/components/system/tool/swaggerView.vue
+++ src/components/system/tool/swaggerView.vue
@@ -1,23 +1,39 @@
-
-
 <template>
-<div></div>
+  <iframe src="http://localhost:8080/swagger-ui.html"  style="width: 100%; height: 100%"/>
 </template>
 
 <script>
 export default {
   created() {
-    this.swagger();
+    setTimeout(() => {
+      var iframe = document.querySelector("#iframe");
+      this.populateIframe(iframe, [["Token", window.sessionStorage.getItem("token")]]);
+    }, 0);
   },
-  methods:{
-    swagger(){
-      this.$http.get('http://localhost:8080');
-    }
-  }
+  methods: {
+    populateIframe(iframe, headers) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", "api/home.chat/chat?type=1");
+      xhr.responseType = "blob";
+      headers.forEach((header) => {
+        xhr.setRequestHeader(header[0], header[1]);
+      });
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === xhr.DONE) {
+          console.log(xhr.response);
+          if (xhr.status === 200) {
+            iframe.src = URL.createObjectURL(xhr.response);
+          }
+        }
+      };
+      xhr.send();
+    },
+  },
+
 }
 </script>
 
 
-<style scoped>
+<style>
 
 </style>
